721add1ac46863f6465a433dc793daa790af20c9
Art Ruiz <a.ruiz.jr@gmail.com>
2021-12-02 02:21:41 -0600

    Added router library. Created Auth & Dashboard components. Added routes.

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,3 +1,14 @@
-const App = () => <div>App</div>;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Auth from "./Auth";
+import Dashboard from "./Dashboard";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/auth" element={<Auth />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
